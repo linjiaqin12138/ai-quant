3d05b2379f9acf4db12d04a70f47c35b901b4ee6
linjiaqin <1243288856@qq.com>
2024-10-14 16:45:13 +0800

    use curl_cffi to query cointime news

--- lib/adapter/news.py
+++ lib/adapter/news.py
@@ -2,6 +2,8 @@ import abc
 import json
 from typing import List, Callable, Any, Literal, Dict, Optional
 from datetime import datetime, timezone
+
+from curl_cffi import requests as curl_requests
 import requests
 
 from ..model import NewsInfo
@@ -93,8 +95,29 @@ def get_news_of_cointime(after: Optional[datetime] = None) -> List[NewsInfo]:
             Raises:
                 requests.exceptions.RequestException: 如果请求失败。
             """
+            # curl 'https://cn.cointime.ai/api/column-items/more?column_id=111&datetime=2024-10-14T08%3A11%3A09.663Z&take=20&order=desc' \
+            #     -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+            #     -H 'accept-language: zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7' \
+            #     -H 'cache-control: max-age=0' \
+            #     -H 'cookie: COINTIME-WEBSITE-XSRF-TOKEN=eyJpdiI6IkJvV2lteTlkN0IrK1BtdmtPTHpLS0E9PSIsInZhbHVlIjoiQ1NKWXBDYkZJL0w1RERqQ3cvVEtjOXBuY3NpZkxQQzFsN1B5Y05UNnUvRUI1NkxnOW9FVnh0UWp0RC82bWNpTkhRb1lRbzFiRDRzKytsNUpjenF4U1FaUmg1Z0JWSTRPby9zZzNYOS9CdXM4S2hXeWh5dWZoSUtiSmNmUGxCS1giLCJtYWMiOiI5OGU4ZGVjYzU2YTc2ZDM4OTQ4OGE1NTU5YWNiMjZkM2E5M2RkN2YyNTFkNzA3NzNiODJjZjUwYzE1OGIxNGNlIiwidGFnIjoiIn0%3D; cointime_website_session=eyJpdiI6IlA5Y2dEbEZOMjl1WlIzTW5oc1VDUEE9PSIsInZhbHVlIjoiNnlFR3hWV01RUmRuUTJOdzVGOGZLVFAreG5BQzNTNlhONDNNT2F0U0hJUXg5cnZRWHR2L1ZZVXh6aC9lcjdxbjh4elpkMmVjNDdtbFdVZUxLN0QyVUViL0t5VnlKVXlYUE5TTDMrZndnKy9mNlU4NWlaVEpiVUlkdERMUXpqUEgiLCJtYWMiOiJjNzBmM2FiODA3MjQ2ZjNhNWE5MTU4MDRkYjM5ZGU3Y2MxZWFjNzFiM2Y4MWVmYmQ2NjE2NGFkNzAzYmE4NTYyIiwidGFnIjoiIn0%3D' \
+            #     -H 'priority: u=0, i' \
+            #     -H 'sec-ch-ua: "Microsoft Edge";v="129", "Not=A?Brand";v="8", "Chromium";v="129"' \
+            #     -H 'sec-ch-ua-mobile: ?0' \
+            #     -H 'sec-ch-ua-platform: "Windows"' \
+            #     -H 'sec-fetch-dest: document' \
+            #     -H 'sec-fetch-mode: navigate' \
+            #     -H 'sec-fetch-site: none' \
+            #     -H 'sec-fetch-user: ?1' \
+            #     -H 'upgrade-insecure-requests: 1' \
+            #     -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'
+            
+            
+
             url = f"https://cn.cointime.ai/api/column-items/more?column_id=111&datetime={url_encode(last_timestamp)}&take=20&order=desc"
-            res = requests.get(url, proxies={ 'all': get_http_proxy() })
+            headers = { 
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'
+            }
+            res = curl_requests.get(url, headers=headers, proxies={ 'http': get_http_proxy(), 'https': get_http_proxy() })
             res.raise_for_status()
             rsp_body = res.json()
             assert rsp_body["code"] == 0
