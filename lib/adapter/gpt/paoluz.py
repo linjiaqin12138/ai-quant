e8896127b83c271a63dc8589de41524a68077796
JiaqinLin <JiaqinLin@outlook.com>
2024-12-31 13:54:48 +0800

    support list models for paoluz

--- lib/adapter/gpt/paoluz.py
+++ lib/adapter/gpt/paoluz.py
@@ -1,6 +1,6 @@
 
 import json
-from typing import Optional, TypedDict
+from typing import Optional, TypedDict, List, Any
 import requests
 
 from ...logger import logger
@@ -8,17 +8,62 @@ from ...config import API_MAX_RETRY_TIMES, get_paoluz_token
 from ...utils.decorators import with_retry
 from .interface import GptAgentAbstract, GptSystemParams
 
+@with_retry((requests.exceptions.ConnectionError, requests.exceptions.Timeout), API_MAX_RETRY_TIMES)
+def retryable_query(method: str, endpoint: str, path: str, token: str, data: str = None):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    logger.debug(f"Paoluz API calling endpoint {endpoint}{path} data: {data}, headers: {headers}")
+    if data:
+        logger.info(f"Paoluz API calling with body size: {len(data)} Byte")
+    if method == 'post':
+        response = requests.post(f'{endpoint}{path}', data=data, headers=headers, stream=False)
+    else:
+        response = requests.get(f'{endpoint}{path}', headers=headers)
+    logger.info(f"Paoluz API calling statusCode: {response.status_code}")
+    logger.debug(f"Paoluz API response header {response.headers}")
+    logger.debug(f"Paoluz API response body {response.text}")
+    return response
+
 class PaoluzAgent(GptAgentAbstract):
     AdditionalOptions = TypedDict('AdditionalOptions', {
         'api_endpoint': Optional[str],
         'token': Optional[str],
     })
-    
+    default_endpoint = "https://chatapi.nloli.xyz"
+    backup_endpoint  = "https://hkc3s.shamiko.uk"
+    system_token = get_paoluz_token()
+
     def __init__(self, model: str = 'gpt-3.5-turbo', system_prompt: Optional[str] = None, system_params: GptSystemParams = {}, **addtional_options: AdditionalOptions):
         super().__init__(model, system_prompt, system_params)
-        self.backup_endpoint = addtional_options.get('api_backup_endpoint', "https://hkc3s.shamiko.uk")
-        self.endpoint = addtional_options.get('api_endpoint', "https://chatapi.nloli.xyz")
-        self.api_key = addtional_options.get('token', get_paoluz_token())
+        self.backup_endpoint = addtional_options.get('api_backup_endpoint', PaoluzAgent.backup_endpoint)
+        self.endpoint = addtional_options.get('api_endpoint', PaoluzAgent.default_endpoint)
+        self.api_key = addtional_options.get('token', PaoluzAgent.system_token)
+    
+    @staticmethod
+    def supported_models() -> List[Any]:
+        path = "/v1/models"
+        rsp = None
+        try:
+            rsp = retryable_query("get", PaoluzAgent.default_endpoint, path, PaoluzAgent.system_token)
+            rsp.raise_for_status()
+        except Exception as e:
+            logger.error(f"Paoluz API calling failed with exception: {e}")
+            rsp = retryable_query("get", PaoluzAgent.backup_endpoint, path, PaoluzAgent.system_token)
+            rsp.raise_for_status()
+        return rsp.json().get('data')
+
+    def _query_with_endpoint_retry(self, method: str, path: str, data: str = None):
+        rsp = retryable_query(method, self.endpoint, path, self.api_key, data)
+        if rsp.status_code != 200:
+            if rsp.status_code == 429 or rsp.status_code >= 500:
+                logger.warning(f"Paoluz API calling failed with statusCode: {rsp.status_code}, retry another endpoint")
+                rsp = retryable_query(method, self.backup_endpoint, path, self.api_key, data)
+                assert rsp.status_code == 200
+            else:
+                raise Exception(f"Paoluz API calling failed with statusCode: {rsp.status_code}, response body: {rsp.text}")
+        return rsp
 
     def ask(self, question: str) -> str:
         self.chat_context.append({"role": "user", "content": question})
@@ -28,33 +73,8 @@ class PaoluzAgent(GptAgentAbstract):
             "stream": False,
         }
         data.update(self.params)
-
         json_data = json.dumps(data, ensure_ascii=False)
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.api_key
-        }
-
-        @with_retry((requests.exceptions.ConnectionError, requests.exceptions.Timeout), API_MAX_RETRY_TIMES)
-        def retryable_query(endpoint: str):
-            logger.debug(f"Paoluz API calling data: {json_data}")
-            logger.info(f"Paoluz API calling with body size: {len(json_data)} Byte")
-            response = requests.post(f'{endpoint}/v1/chat/completions', data=json_data, headers=headers, stream=False)
-            logger.info(f"Paoluz API calling statusCode: {response.status_code}")
-            logger.debug(f"Paoluz API response header {response.headers}")
-            logger.debug(f"Paoluz API response body {response.text}")
-            return response
-
-        rsp = retryable_query(self.endpoint)
-
-        if rsp.status_code != 200:
-            if rsp.status_code == 429 or rsp.status_code >= 500:
-                logger.warning(f"Paoluz API calling failed with statusCode: {rsp.status_code}, retry another endpoint")
-                rsp = retryable_query(self.backup_endpoint)
-                assert rsp.status_code == 200
-            else:
-                raise Exception(f"Paoluz API calling failed with statusCode: {rsp.status_code}")
-
+        rsp = self._query_with_endpoint_retry("post", "/v1/chat/completions", json_data)
         rsp_body = rsp.json()
         rsp_message = rsp_body["choices"][0]["message"]["content"]
         self.chat_context.append({"role": "assistant", "content": rsp_message})
